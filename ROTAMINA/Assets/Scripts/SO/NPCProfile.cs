a20c1985da16f6e5f3ae969d2bf546ff332ad16b
lentilek <laura.robinska@wp.pl>
2025-07-07 14:57:15 +0200

    nps profiles & animaajusted

--- ROTAMINA/Assets/Scripts/SO/NPCProfile.cs
+++ ROTAMINA/Assets/Scripts/SO/NPCProfile.cs
@@ -13,4 +13,5 @@ public class NPCProfile : ScriptableObject
     public OptionSO age, zodiac, gender, personality;
     public List<Likes> likes = new List<Likes>(), dislikes = new List<Likes>();
     public string startingDialogue;
+    public Sprite polaroid;
 }
